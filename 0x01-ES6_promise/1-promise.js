1bb81c0ebbad0be179f116d2a39e7dec0ac9b996
Dinobi <architectstan01@gmail.com>
2023-07-26 16:07:29 +0100

    ES6 Javascript Promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
-export default getFullResponseFromAPI(success) {
-  return (new Promise(function(resolve, reject) {
+export default function getFullResponseFromAPI(success) {
+  return new Promise(function(resolve, reject) {
   if (success) {
     resolve({ status: 200, body: 'Success' });
   } else {
     reject(new Error('The fake API is not working currently'));
   }
   })
-)}
+}
