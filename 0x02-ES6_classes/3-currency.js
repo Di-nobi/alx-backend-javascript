d520d3acf0beca3cfe4b0f48d7ba2aa98046b088
Dinobi <architectstan01@gmail.com>
2023-07-28 04:18:48 +0100

    ES6 Javascript

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,37 @@
+export default class Currency {
+  constructor(code, name) {
+  if (typeof name !== 'string') {
+    throw TypeError('name must be a string');
+  }
+  if (typeof code !== 'string') {
+    throw TypeError('code must a string');
+  }
+  this._code = code;
+  this._name = name;
+  }
+  get name() {
+  return this._name;
+  }
+
+  get code() {
+  return this._code;
+  }
+
+  set name(arg) {
+    if (typeof arg !== 'string') {
+      throw TypeError('Must be a string input');  
+    }
+    this._name = arg;
+  }
+
+  set code(arg1) {
+    if (typeof arg1 !== 'string') {
+      throw TypeError('Must be a string input');
+    }
+    this._code = arg1;
+  }
+
+  displayFullCurrency() {
+  return `${this._name} (${this._code})`;
+  }
+}
