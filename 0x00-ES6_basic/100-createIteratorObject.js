0ce0a6e08b67ead1afa3f718a68cb8c3d0234606
Dinobi <architectstan01@gmail.com>
2023-07-29 15:29:26 +0100

    ES6 Javascript

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 export default function createIteratorObject(report) {
   let arrval = [];
-  for (const idx of Object.values( report.allEmployees)) {
+  for (const idx of Object.values(report.allEmployees)) {
     arrval.push(...idx);
   }
   return arrval;
