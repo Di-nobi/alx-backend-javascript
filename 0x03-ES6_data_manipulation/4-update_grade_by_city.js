2759831039789d9c04f87058dcf70490a9081e6f
Dinobi <architectstan01@gmail.com>
2023-08-24 20:43:00 +0100

    Backend JavaScript

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,6 +2,14 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   if (!Array.isArray(newGrades) || !Array.isArray(students)) {
     return []
   }
-  const eachstudent = students.filter((student) => student.location === city);
+  const eachstudent = students.filter((student) => student.location === city).map((student) => {
+  if (grade) {
+    student.grade = grade.grade;
+  } else {
+    student.grade = 'N/A';
+  }
+  return student;
+  })
+  return eachstudent;
   
 }
