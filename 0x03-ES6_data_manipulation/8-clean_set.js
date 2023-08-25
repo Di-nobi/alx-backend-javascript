d529035a98c66c163df283ab3f84fa99cf6935b2
Dinobi <architectstan01@gmail.com>
2023-08-26 00:13:11 +0100

    Backend JavaScript

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,9 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
-  let getSet = [...set]
+  const getSet = [...set];
   return getSet
-  .filter((value) => value.startsWith(startString))
-  .map((value) => value.slice(startString.length))
-  .join('-');
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
 }
