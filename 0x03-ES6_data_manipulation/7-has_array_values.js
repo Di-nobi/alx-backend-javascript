d402a25fb4eb531a08999f545bb1b81feb9f0e2c
Dinobi <architectstan01@gmail.com>
2023-08-25 23:31:05 +0100

    Backend JavaScript

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,8 @@
-export default function hasValuesFromArray
+export default function hasValuesFromArray(set, array){
+  for (let value of array) {
+    if (!set.has(value)) {
+      return false;
+    }
+  }
+  return true;
+}
