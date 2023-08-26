7f64dab8f143ffad79341c1d0e16ad8f08345344
Dinobi <architectstan01@gmail.com>
2023-08-26 08:23:06 +0100

    Backend JavaScript

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const mapout = new Map();
+  mapout.set('Apples', 10);
+  mapout.set('Tomatoes', 10);
+  mapout.set('Pasta', 1);
+  mapout.set('Rice', 1);
+  mapout.set('Banana', 5);
+  return mapout;
+}
