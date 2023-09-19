761e569b5420cf9e83d9cb8bd1a2c6be2cd03314
Dinobi <architectstan01@gmail.com>
2023-09-19 23:16:06 +0100

    Nodejs project

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,10 +3,18 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const figs = data.toString().split('\n').split(',');
-    const students = [];
-    figs.forEach((data) => {
-      students.push(data);
+    const figs = data.slice(1).split('\n');
+    fields = []
+    // const students = [];
+    // figs.forEach((data) => {
+    //   students.push(data);
+    // });
+    // students.slice(1);
+    const students = figs.map((fig) => fig.split(','));
+    students.forEach((data) => {
+      const [first, last, age, field] = data;
+      if (first && last && age && field) {
+        fields.push({first, last, age, field});
+      }
     });
-    students.slice(1);
-    
+  }
