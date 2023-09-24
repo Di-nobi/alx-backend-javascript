925dc091c8cac3b1a7b2f74f470df276e8f165ef
Dinobi <architectstan01@gmail.com>
2023-09-24 07:13:45 +0100

    Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,18 +3,29 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const figs = data.slice(1).split('\n');
-    fields = []
-    // const students = [];
-    // figs.forEach((data) => {
-    //   students.push(data);
-    // });
-    // students.slice(1);
-    const students = figs.map((fig) => fig.split(','));
-    students.forEach((data) => {
-      const [first, last, age, field] = data;
-      if (first && last && age && field) {
-        fields.push({first, last, age, field});
+    const figs = data.slice(1).toString().split('\n');
+    console.log(`Number of students: ${figs.length - 1}`);
+    const field = {};
+    // const students = {};
+    for (let count = 0; count < figs.length; count += 1) {
+      const dema = figs[count].split(',');
+      if (field[dema[3]] == null) {
+        field[dema[3]] = [];
       }
-    });
+      else {
+        // field[dema[3]] = [];
+        field[dema[3]].push(dema[0]);
+      }
+    }
+    for (let aField in field) {
+      if (field.hasOwnProperty(aField)) {
+        let getlen = field[aField].length;
+        let students = field[aField].join(',');
+        console.log(`Number of students is ${aField}: ${getlen}. List: ${students}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+    }
   }
+  module.exports = countStudents;
