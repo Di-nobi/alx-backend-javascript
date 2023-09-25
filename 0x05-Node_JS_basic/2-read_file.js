730a73e12257767bf62dc2520e33c7d8cb4fd442
Dinobi <architectstan01@gmail.com>
2023-09-25 21:47:16 +0100

    Node Js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -12,22 +12,22 @@ function countStudents(path) {
       const dema = figs[count].split(',');
       if (field[dema[3]]) {
         field[dema[3]].push(dema[0]);
+      } else {
+        field[dema[3]] = [dema[0]];
       }
-      else {
-        field[dema[3]] = [dema[0]];  
-      }
-    };
+    }
 
-    for (let aField in field) {
-      if (field.hasOwnProperty(aField)) {
-        let getlen = field[aField].length;
-        let students = field[aField].join(', ');
+    for (const aField in field) {
+      if (aField) {
+        const getlen = field[aField].length;
+        const students = field[aField].join(', ');
         console.log(`Number of students in ${aField}: ${getlen}. List: ${students}`);
       }
-    };
+    }
   } catch (error) {
     console.error("Error:", error);
-    throw new Error('Cannot load the database');
-    }
+      throw new Error('Cannot load the database');
+      }
   };
   module.exports = countStudents;
+  
