7342f1e513ca0250ad6c7266d86378b5b4805bba
Dinobi <architectstan01@gmail.com>
2023-09-25 16:28:46 +0100

    Node Js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,32 +1,36 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-function countStudents(path) {
+async function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf-8');
+    const data = await fs.readFile(path, 'utf-8');
+    if (data == null) {
+        throw Error('Cannot load the database');
+    }
     const figs = data.toString().split('\n');
     console.log(`Number of students: ${figs.length - 1}`);
     const field = {};
     // total = 0;
     // const students = {};
     for (let count = 1; count < figs.length; count += 1) {
-      const dema = figs[count].split(',');
-      if (field[dema[3]]) {
+        const dema = figs[count].split(',');
+        if (field[dema[3]]) {
         field[dema[3]].push(dema[0]);
-      }
-      else {
+        }
+        else {
         field[dema[3]] = [dema[0]];  
-      }
+        }
     };
-
+    
     for (let aField in field) {
-      if (field.hasOwnProperty(aField)) {
+        if (field.hasOwnProperty(aField)) {
         let getlen = field[aField].length;
         let students = field[aField].join(', ');
         console.log(`Number of students in ${aField}: ${getlen}. List: ${students}`);
-      }
+        }
     };
-  } catch (error) {
-    throw new Error('Cannot load the database');
+}   catch (error) {
+    console.error(error.message);
     }
-  };
-  module.exports = countStudents;
+}
+
+module.exports = countStudents;
