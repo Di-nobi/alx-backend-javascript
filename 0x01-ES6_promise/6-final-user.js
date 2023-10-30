35923eca9a48a0e449e071d8d42c0291a3c3b882
Dinobi <architectstan01@gmail.com>
2023-10-30 07:47:19 +0100

    ES6-promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,23 @@
 import { SignUpUser } from './4-user-promise';
 import { uploadPhoto } from './5-photo-reject';
 
-export default function(firstName, lastName, fileName) {
-  let 
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const [user, photo] = await Promise.allSettled([SignUpUser(firstName, lastName), uploadPhoto(fileName)]);
+    const process = [
+      {
+        status: user.status,
+        value: user.status === 'fulfilled' ? user.value : user.reason.toString(),
+      },
+
+      {
+        status: photo.status,
+        value: photo.status === 'fulfilled' ? photo.value : photo.reason.toString(),
+      }
+    ];
+    return process;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 }
