2a2788b0900d61d68d139cb38a9f34ae699c35d9
Dinobi <architectstan01@gmail.com>
2023-10-30 08:50:59 +0100

    ES6-promise

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,14 @@
-import { uploadPhoto, creatUser } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
     let dataPic;
     let usr;
     try {
         dataPic = await uploadPhoto();
-        usr = await creatUser();
+        usr = await createUser();
     } catch (err) {
-        dataPic = null;
-        usr = null;
+      dataPic = null;
+      usr = null;
     }
 
     return { dataPic, usr };
