4b29b4b382a328e14787fef1c938a04744dd5c23
Dinobi <architectstan01@gmail.com>
2023-11-07 00:01:36 +0100

    Node js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,45 +1,48 @@
 const http = require('http');
 const fs = require('fs');
-
+const util = require('util');
 port = 1245;
 
 hostname = 'localhost';
-
-async function countStudents(path) {
+const readFileAsync = util.promisify(fs.readFile);
+async function countStudents(fileName) {
     try {
-      const data = await fs.readFile(path, 'utf-8');
-      if (data == null) {
-          throw Error('Cannot load the database');
+      const data = await readFileAsync(fileName, 'utf8');
+      const students = {};
+      const fields = {};
+  
+      const lines = data.split('\n').filter(line => line.trim() !== '');
+  
+      lines.forEach(line => {
+        const [name, id, email, field] = line.split(',').map(item => item.trim());
+  
+        if (students[field]) {
+          students[field].push(name);
+        } else {
+          students[field] = [name];
+        }
+  
+        if (fields[field]) {
+          fields[field]++;
+        } else {
+          fields[field] = 1;
+        }
+      });
+  
+      const totalStudents = lines.length - 1;
+      let output = `Number of students: ${totalStudents}\n`;
+  
+      for (const [key, value] of Object.entries(fields)) {
+        output += `Number of students in ${key}: ${value}. `;
+        output += `List: ${students[key].join(', ')}\n`;
       }
-      const figs = data.trim().split('\n');
-      const output = figs.length - 1
-      message = `Number of students: ${output}`;
-      console.log(message);
-      const field = {};
-      // total = 0;
-      // const students = {};
-      for (let count = 1; count < figs.length; count += 1) {
-          const dema = figs[count].split(',');
-          if (field[dema[3]]) {
-          field[dema[3]].push(dema[0]);
-          }
-          else {
-          field[dema[3]] = [dema[0]];  
-          }
-      };
-      
-      for (let aField in field) {
-          if (field.hasOwnProperty(aField)) {
-          let getlen = field[aField].length;
-          let students = field[aField].join(', ');
-          console.log(`Number of students in ${aField}: ${getlen}. List: ${students}`);
-          }
-      };
-      return { message };
-      }   catch (error) {
-      console.error(error.message);
-      }
+  
+      return output;
+    } catch (err) {
+      throw err;
+    }
   }
+  
 app = http.createServer((req, res) => {
     res.statusCode == 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -49,10 +52,10 @@ app = http.createServer((req, res) => {
     }
     if (req.url === '/students') {
         res.write('This is the list of our students\n');
-        countStudents(process.argv[2])
+        countStudents(process.argv[2].toString())
         .then((data) => {
-            res.write(data.message);
-            res.end();
+            const output = data.slice(0, -1);
+            res.end(output);
         });
     }
 });
