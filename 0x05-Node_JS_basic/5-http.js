8ae6c7270753e2439560eeeada294765dc8bb3c3
Dinobi <architectstan01@gmail.com>
2023-11-07 00:05:36 +0100

    Node js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,47 +1,92 @@
 const http = require('http');
-const fs = require('fs');
-const util = require('util');
-port = 1245;
+const { readFile } = require('fs');
 
-hostname = 'localhost';
-const readFileAsync = util.promisify(fs.readFile);
-async function countStudents(fileName) {
-    try {
-      const data = await readFileAsync(fileName, 'utf8');
-      const students = {};
-      const fields = {};
-  
-      const lines = data.split('\n').filter(line => line.trim() !== '');
-  
-      lines.forEach(line => {
-        const [name, id, email, field] = line.split(',').map(item => item.trim());
-  
-        if (students[field]) {
-          students[field].push(name);
-        } else {
-          students[field] = [name];
+const hostname = 'localhost';
+const port = 1245;
+
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
         }
-  
-        if (fields[field]) {
-          fields[field]++;
-        } else {
-          fields[field] = 1;
+        output += `Number of students: ${length - 1}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
         }
-      });
-  
-      const totalStudents = lines.length - 1;
-      let output = `Number of students: ${totalStudents}\n`;
-  
-      for (const [key, value] of Object.entries(fields)) {
-        output += `Number of students in ${key}: ${value}. `;
-        output += `List: ${students[key].join(', ')}\n`;
+        resolve(output);
       }
+    });
+  });
+}
+// const http = require('http');
+// const fs = require('fs');
+// const util = require('util');
+// port = 1245;
+
+// hostname = 'localhost';
+// const readFileAsync = util.promisify(fs.readFile);
+// async function countStudents(fileName) {
+//     try {
+//       const data = await readFileAsync(fileName, 'utf8');
+//       const students = {};
+//       const fields = {};
   
-      return output;
-    } catch (err) {
-      throw err;
-    }
-  }
+//       const lines = data.split('\n').filter(line => line.trim() !== '');
+  
+//       lines.forEach(line => {
+//         const [name, id, email, field] = line.split(',').map(item => item.trim());
+  
+//         if (students[field]) {
+//           students[field].push(name);
+//         } else {
+//           students[field] = [name];
+//         }
+  
+//         if (fields[field]) {
+//           fields[field]++;
+//         } else {
+//           fields[field] = 1;
+//         }
+//       });
+  
+//       const totalStudents = lines.length - 1;
+//       let output = `Number of students: ${totalStudents}\n`;
+  
+//       for (const [key, value] of Object.entries(fields)) {
+//         output += `Number of students in ${key}: ${value}. `;
+//         output += `List: ${students[key].join(', ')}\n`;
+//       }
+  
+//       return output;
+//     } catch (err) {
+//       throw err;
+//     }
+//   }
   
 app = http.createServer((req, res) => {
     res.statusCode == 200;
