a78544e46f4fad104087d7845bb2df5f15cad64a
Dinobi <architectstan01@gmail.com>
2023-11-07 12:00:04 +0100

    ES6

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -4,11 +4,29 @@ export default class HolbertonClass {
     this._location = location;
   }
 
-  ValueOf() {
+  set size(size) {
+    if (typeof size !== 'number') {
+      throw TypeError('size must be a number');
+    }
+    this._size = size;
+  }
+
+  get size() {
     return this._size;
   }
 
-  toString() {
+  set location(location) {
+    if (typeof location !== 'string') {
+      throw TypeError('location must be a string');
+    }
+    this._location = location;
+  }
+
+  get location() {
     return this._location;
+  }
+
+  [Symbol.toPrimitive](mtt) {
+    return mtt === 'number' ? this._size : this._location;
   }
 }
