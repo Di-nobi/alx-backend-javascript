bf39169c34bcc04c332be45c14f19f6d28498618
Dinobi <architectstan01@gmail.com>
2023-11-07 06:35:34 +0100

    Data

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -28,38 +28,5 @@ async function countStudents(path) {
     };
   }
 
-// async function countStudents(path) {
-//   try {
-//     const data = await fs.readFile(path, 'utf-8');
-//     if (data == null) {
-//         throw Error('Cannot load the database');
-//     }
-//     const figs = data.trim().split('\n');
-//     const output = figs.length - 1
-//     console.log(`Number of students: ${output}`);
-//     const field = {};
-//     // total = 0;
-//     // const students = {};
-//     for (let count = 1; count < figs.length; count += 1) {
-//         const dema = figs[count].split(',');
-//         if (field[dema[3]]) {
-//         field[dema[3]].push(dema[0]);
-//         }
-//         else {
-//         field[dema[3]] = [dema[0]];  
-//         }
-//     };
-    
-//     for (let aField in field) {
-//         if (field.hasOwnProperty(aField)) {
-//         let getlen = field[aField].length;
-//         let students = field[aField].join(', ');
-//         console.log(`Number of students in ${aField}: ${getlen}. List: ${students}`);
-//         }
-//     };
-//     }   catch (error) {
-//     console.error(error.message);
-//     }
-// }
 
 module.exports = countStudents;
