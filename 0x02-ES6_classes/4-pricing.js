36cced6fb3bbe08493686e3839bb3cd524ad62da
Dinobi <architectstan01@gmail.com>
2023-11-07 12:24:18 +0100

    ES6

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,39 +1,38 @@
-import Currency  from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-  if (typeof amount !== 'number') {
-    throw TypeError('Value was be a number');
-  }
-  this._amount = amount;
-  this._currency = currency;
+    if (typeof amount !== 'number') {
+      throw TypeError('Value was be a number');
+    }
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
-  return this._amount;
+    return this._amount;
   }
 
   get currency() {
-  return this._currency;
+    return this._currency;
   }
 
   set amount(arg) {
-  if (typeof arg !== 'number') {
-    throw TypeError('Not a number');
-  }
-  this._amount = arg;
+    if (typeof arg !== 'number') {
+      throw TypeError('Not a number');
+    }
+    this._amount = arg;
   }
 
   set currency(arg1) {
-
-  this._currency = arg1;
+    this._currency = arg1;
   }
 
   displayFullPrice() {
-  return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
-  return amount * conversionRate;
+    return amount * conversionRate;
   }
 }
