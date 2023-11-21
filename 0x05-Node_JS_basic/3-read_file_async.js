a4fdc42b0b0203c0cdf6ea16c56be9785afc7d23
Dinobi <architectstan01@gmail.com>
2023-11-21 23:22:45 +0100

    Updated

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,33 +1,37 @@
 const fs = require('fs').promises;
 
 async function countStudents(path) {
-    try {
-      const data = await fs.readFile(path, 'utf-8');
-      const figs = data.toString().split('\n');
-      const stds = figs.slice(1).filter((fig) => fig.trim().length > 0).map((fig) => fig.split(','));
-  
-      const fields = {};
-      stds.forEach((std) => {
-        const [firstName,,, field] = std;
-  
-        if (!fields[field]) {
-          fields[field] = { count: 0, names: [] };
-        }
+  try {
+    const data = await fs.readFile(path, { encoding: 'utf8' });
 
-        /* eslint-disable no-plusplus */
-        fields[field].count++;
-        fields[field].names.push(firstName);
-      });
-  
-      console.log(`Number of students: ${stds.length}`);
-  
-      Object.keys(fields).forEach((field) => {
-        console.log(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].names.join(', ')}`);
-      });
-    } catch (err) {
-      console.log(err.message);
-    };
+    if (!data) {
+      throw new Error('Cannot load the database');
+    }
+
+    const lines = data.trim().split('\n');
+    const students = lines.slice(1).filter((line) => line.trim().length > 0).map((line) => line.split(','));
+
+    const fieldCounts = {};
+    students.forEach((student) => {
+      const [firstName,,, field] = student;
+
+      if (!fieldCounts[field]) {
+        fieldCounts[field] = { count: 0, names: [] };
+      }
+
+      /* eslint-disable no-plusplus */
+      fieldCounts[field].count++;
+      fieldCounts[field].names.push(firstName);
+    });
+
+    console.log(`Number of students: ${students.length}`);
+
+    Object.keys(fieldCounts).forEach((field) => {
+      console.log(`Number of students in ${field}: ${fieldCounts[field].count}. List: ${fieldCounts[field].names.join(', ')}`);
+    });
+  } catch (error) {
+    console.error(error.message);
   }
-  
-module.exports = countStudents;
+}
 
+module.exports = countStudents;
