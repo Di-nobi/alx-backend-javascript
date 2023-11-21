ea53df0caac12b6e49c0a95725840207ef841e88
Dinobi <architectstan01@gmail.com>
2023-11-21 15:15:39 +0100

    tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,31 +1,20 @@
-const api = require('./api');
-const chai = require('chai');
-const expect = chai.expect;
 const request = require('request');
-const {describe, it} = require("mocha");
+const { expect } = require('chai');
 
-describe('GET /', function () {
-    const datas = {
+
+describe('Integration Testing', () => {
+  describe('GET /', () => {
+    it('Code: 200 | Body: Welcome to the payment system', (done) => {
+      const options = {
         url: 'http://localhost:7865',
-        method: 'GET'
-    }
-    it('Tests the api', function (done) {
+        method: 'GET',
+      };
 
-        request(datas, (err, res, body) => {
-            expect(res.statusCode).to.equal(200);
-            done();
-        });
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
-    it('checks for the correct content', function(done) {
-        request(datas, (err, res, body) => {
-            expect(body).to.contain("Welcome to the payment system");
-            done();
-        });
-    });
-    it('checks for the content length', function(done) {
-        request(datas, (err, res, body) => {
-            expect(res.headers['content-length']).to.equal('29');
-            done();
-        });
-    });
+  });
 });
