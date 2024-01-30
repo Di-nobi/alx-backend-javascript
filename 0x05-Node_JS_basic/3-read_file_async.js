3084cdf2e9fd5e4d6e66ba9605d4ada35cf6fb83
Dinobi <architectstan01@gmail.com>
2024-01-30 10:58:34 +0100

    Nodejs basics

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,28 +1,28 @@
 const fs = require('fs').promises;
 
 async function countStudents(path) {
-    try {
+  try {
     const data = await fs.readFile(path, { encoding: 'utf-8' });
     const line = data.split('\n');
 
     const csli = [];
     const sWE = [];
+    /* eslint-disable no-plusplus */
     for (let i = 0; i < line.length; i++) {
-        const datas = line[i].split(',');
-        const mydata = datas[3];
-
-        if (mydata == 'CS') {
-            csli.push(datas[0]);
-        } else if (mydata == 'SWE') {
-            sWE.push(datas[0]);
-        }
+      const datas = line[i].split(',');
+      const mydata = datas[3];
+      if (mydata === 'CS') {
+        csli.push(datas[0]);
+      } else if (mydata === 'SWE') {
+        sWE.push(datas[0]);
+      }
     }
     console.log(`Number of students: ${line.length - 1}`);
     console.log(`Number of students in CS: ${csli.length}. List: ${csli.join(', ')}`);
     console.log(`Number of students in SWE: ${sWE.length}. List: ${sWE.join(', ')}`);
-} catch (err) {
-    throw new Error(`Cannot load the database`);
-}
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
