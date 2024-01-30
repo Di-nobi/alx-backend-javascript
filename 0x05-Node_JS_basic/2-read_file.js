f1feca608fa9a77396449a6f3c2e20eb2133d8d8
Dinobi <architectstan01@gmail.com>
2024-01-30 10:32:39 +0100

    Nodejs basics

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,27 +2,26 @@ const fs = require('fs');
 
 function countStudents(path) {
     try {
-    const data = fs.readFileSync(path, { encoding: 'utf-8' });
-    const line = data.split('\n');
+      const data = fs.readFileSync(path, { encoding: 'utf-8' });
+      const line = data.split('\n');
+      const csli = [];
+      const sWE = [];
+      for (let i = 0; i < line.length; i++) {
+          const datas = line[i].split(',').trim();
+          const mydata = datas[3];
 
-    const csli = [];
-    const sWE = [];
-    for (let i = 0; i < line.length; i++) {
-        const datas = line[i].split(',').trim();
-        const mydata = datas[3];
-
-        if (mydata == 'CS') {
+          if (mydata == 'CS') {
             csli.push(datas[0]);
-        } else if (mydata == 'SWE') {
-            sWE.push(datas[0]);
+          } else if (mydata == 'SWE') {
+              sWE.push(datas[0]);
+            }
         }
+        console.log(`Number of students: ${line.length - 1}`);
+        console.log(`Number of students in CS: ${csli.length}. List: ${csli.join(', ')}`);
+        console.log(`Number of students in SWE: ${sWE.length}. List: ${sWE.join(', ')}`);
+    } catch (err) {
+      throw new Error('Cannot load the database');
     }
-    console.log(`Number of students: ${line.length - 1}`);
-    console.log(`Number of students in CS: ${csli.length}. List: ${csli.join(', ')}`);
-    console.log(`Number of students in SWE: ${sWE.length}. List: ${sWE.join(', ')}`);
-} catch (err) {
-    throw new Error(`Cannot load the database`);
-}
 }
 
 module.exports = countStudents;
