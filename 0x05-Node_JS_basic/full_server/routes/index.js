45f28bf7fce59cea094e29a14bace43c373a3614
Dinobi <architectstan01@gmail.com>
2024-01-31 01:10:14 +0100

    Nodejs Basics

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -5,11 +5,11 @@ const StudentsController = require('../controllers/StudentsController');
 const app = express();
 
 app.get('/', (req, res) => {
-    AppController.getHomepage(req, res);
+  AppController.getHomepage(req, res);
 });
 
 app.get('/students', (req, res) => {
-    StudentsController.getAllStudents(req, res);
+  StudentsController.getAllStudents(req, res);
 });
 
 module.exports = app;
